1a169359a3fedff727a25c9bc2ed248dac5c7eef
Ignacio Maximiliano Jimenez Ramirez <pub.maximilianojimenez@gmail.com>
2024-06-07 11:33:21 -0600

    Adding short selling strategy -Maximiliano Jimenez

--- technical_analysis/main.py
+++ technical_analysis/main.py
@@ -94,6 +94,73 @@ plt.plot(portfolio_value_benchmark, label="Passive")
 plt.legend()
 plt.show()
 
+## Short Selling
+
+# Señal de venta basada en el RSI
+technical_data["SELL_SIGNAL"] = (technical_data.RSI > 75) & (technical_data.RSI2 > 75)
+
+capital = 1_000_000
+n_shares = 100
+stop_loss = 0.4
+take_profit = 0.4
+COM = 0.125 / 100
+
+active_positions = []
+portfolio_value = [capital]
+
+for i, row in technical_data.iterrows():
+    # Cerrar todas las posiciones que han alcanzado el stop loss o el take profit
+    active_pos_copy = active_positions.copy()
+    for pos in active_pos_copy:
+        if row.Close > pos["stop_loss"]:  # La posición se cierra con pérdida
+            capital += (pos["sold_at"] - row.Close) * pos["n_shares"] * (1 - COM)
+            active_positions.remove(pos)
+        elif row.Close < pos["take_profit"]:  # La posición se cierra con ganancia
+            capital += (pos["sold_at"] - row.Close) * pos["n_shares"] * (1 - COM)
+            active_positions.remove(pos)
+
+    # Verificar si hay señal de venta
+    if row.SELL_SIGNAL:
+        # Verificar si tenemos suficientes acciones para vender
+        if (capital > row.Close * (1 + COM) * n_shares * 1.5) and len(active_positions) < 1000:
+            capital -= row.Close * (COM) * n_shares
+            active_positions.append({
+                "type": "SHORT",
+                "sold_at": row.Close,
+                "n_shares": n_shares,
+                "stop_loss": row.Close * (1 + stop_loss),
+                "take_profit": row.Close * (1 - take_profit)
+            })
+        else:
+            print("OUT OF CASH")
+
+    # Valor del portafolio a través del tiempo
+    positions_value = sum([(pos["sold_at"] - row.Close) * pos["n_shares"] for pos in active_positions])
+    portfolio_value.append(capital + positions_value)
+
+# Cerrar todas las posiciones restantes al final
+for pos in active_positions.copy():
+    capital += (pos["sold_at"] - row.Close) * pos["n_shares"] * (1 - COM)
+    active_positions.remove(pos)
+
+portfolio_value.append(capital)
+
+
+capital_benchmark = 1_000_000
+shares_to_buy = capital_benchmark // (technical_data.Close.values[0] * (1 + COM))
+capital_benchmark -= shares_to_buy * row.Close * (1 + COM)
+portfolio_value_benchmark = (shares_to_buy * technical_data.Close) + capital_benchmark
+
+plt.title(f"Active={(portfolio_value[-1] / 1_000_000 - 1)*100}%\n" +
+          f"Passive={(portfolio_value_benchmark.values[-1] / 1_000_000 - 1)*100}%")
+plt.plot(portfolio_value, label="Active")
+plt.plot(portfolio_value_benchmark, label="Passive")
+plt.legend()
+plt.show()
+
+
+
+
 ### Optimization
 
 import optuna
